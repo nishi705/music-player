c04fca29ce43caa474c0bfec62388958a65478f0
nishi.singh <nishi.singh@cgi.com>
2019-08-11 22:50:01 +0530

    music player completed

--- docker-trackservice-mongodb1-master/src/main/java/com/stackroute/TrackServiceApplication.java
+++ docker-trackservice-mongodb1-master/src/main/java/com/stackroute/TrackServiceApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 //@PropertySource("classpath:application-dev.properties")
 public class TrackServiceApplication {
